5a4b46f23a09ddac7c6358756049f8842d9448ff
Raghu <raghu@aplopio.com>
2014-05-27 16:12:10 +0530

    cleaning up all code. Rebuilding with new assumptions

--- whistle/exceptions.py
+++
@@ -1,9 +0,0 @@
-
-class InvalidCallable(Exception):
-    '''
-    Raised when an invalid callable object is registered with api
-    callable is invalid when 
-     1. its name is invalid.
-     2. it does not have an _meta attribute with a name parameter
-    '''
-    pass
--- whistle/utils.py
+++
@@ -1,9 +0,0 @@
-import re
-
-alphanum_regex = re.compile('^\w+$')
-def is_alphanum(string):
-    if alphanum_regex.match(string):
-        return True
-    else:
-        return False
-
