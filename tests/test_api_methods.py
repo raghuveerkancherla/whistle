dc0707b2e0557b66eebc319e11f0c613b308313c
Raghu <raghu@aplopio.com>
2014-07-03 15:25:22 +0530

    bugfix: renamed api_method to api_methods

--- tests/test_api_methods.py
+++ tests/test_api_methods.py
@@ -1,6 +1,6 @@
 import unittest
 from mock import MagicMock
-from whistle.api_method import ApiMethod
+from whistle.api_methods import ApiMethod
 from whistle.request import Request
 from whistle.response import Response
 
